7e475b7212ea94ac38cccf4c773c746f629a3aea
RioLynn <wenqi.lin@uconn.edu>
2017-03-31 10:09:56 +0800

    add variables in holes class

--- src/Holes.java
+++ src/Holes.java
@@ -1,4 +1,9 @@
 
 public class Holes {
-
+	BufferedImage image;
+	int x; int y; // (x,y) is the center of two columns
+	int width; 
+	int depth;
+	random r = new random;
+	int distance = 245;//the distance between two holes
 }
